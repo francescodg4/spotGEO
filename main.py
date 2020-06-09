03fe617613220d863b99ee94dd274be8aa6d8515
francescodg4 <francescodg4@gmail.com>
2020-06-09 19:32:12 +0200

    Added function to create the submission file

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import json
+import datetime
 
-submission = []
 
-for sequence_id in range(1, 5121):
-    for frame in range(1, 6):
-        submission.append(
-            {"sequence_id": sequence_id, "frame": frame, "num_objects": 0, "object_coords": []}
-        )
+def create_submission_file():
+    """Write the submission file"""
 
-with open("my_submission.json", "w") as outfile:
-    json.dump(submission, outfile)
+    submission = []
+
+    for sequence_id in range(1, 5121):
+        for frame in range(1, 6):
+            submission.append(
+                {
+                    "sequence_id": sequence_id,
+                    "frame": frame,
+                    "num_objects": 0,
+                    "object_coords": [],
+                }
+            )
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%m-%S")
+
+    with open(f"sub_{timestamp}.json", "w") as outfile:
+        json.dump(submission, outfile)
+
+
+def main(): ...
+
+
+if __name__ == "__main__":
+    main()
